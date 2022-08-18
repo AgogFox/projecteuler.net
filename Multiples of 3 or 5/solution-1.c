bb9fd9e24f7e1550fc0b3c0137528be9c4af5b21
AgogFox <bopawat@gmail.com>
2022-08-18 11:15:23 +0700

    finish solution-1

--- Multiples of 3 or 5/solution-1.c
+++ Multiples of 3 or 5/solution-1.c
@@ -2,6 +2,12 @@
 int main () {
     int x;
     for(int i; i < 1000; i++) {
-        
+        if (3 * i <= 1000) {
+            x += 3 * i;
+        }
+        if (5 * i <= 1000) {
+            x += 5 * i;
+        }
     }
+    printf("%d", x);
 }
