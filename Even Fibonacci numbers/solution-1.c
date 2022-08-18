57e55ac48f08774fe2b8c54b6498ac6fb413b078
AgogFox <bopawat@gmail.com>
2022-08-18 13:11:25 +0700

    finish fibo sol-1

--- Even Fibonacci numbers/solution-1.c
+++ Even Fibonacci numbers/solution-1.c
@@ -1,4 +1,13 @@
 #include<stdio.h>
 int main () {
-    
+    int t1 = 1, t2 = 2, next, count = 0;
+    while (t2 < 4000000) {
+        if (t2 % 2 == 0) {
+            count += t2;
+        }
+        next = t1 + t2;
+        t1 = t2;
+        t2 = next;
+    }
+    printf("%d", count);
 }
