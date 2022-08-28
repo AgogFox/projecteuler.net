d2cd0bcbbbc24c0315b2d55dc7a497fd83eef510
AgogFox <bopawat@gmail.com>
2022-08-28 23:54:54 +0700

    finish fibo sol-2

--- 2.Even Fibonacci numbers/solution-2.c
+++ 2.Even Fibonacci numbers/solution-2.c
@@ -1,4 +1,15 @@
 #include<stdio.h>
 int main () {
-    
+    int fib[3], sum = 0;
+    fib[0] = 1;
+    fib[1] = 2;
+    while (fib[1] < 4000000) {
+        if (fib[1] % 2 == 0) {
+            sum += fib[1];
+        }
+        fib[2] = fib[0] + fib[1];
+        fib[0] = fib[1];
+        fib[1] = fib[2];
+    }
+    printf("%d", sum);
 }
